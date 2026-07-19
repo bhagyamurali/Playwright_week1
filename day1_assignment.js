2c966e4a581073b5c6c3b034b91198edae726014
bhagyamurali <56382308+bhagyamurali@users.noreply.github.com>
2026-07-19 13:27:50 +0530

    Add files via upload

---
+++ day1_assignment.js
@@ -0,0 +1,18 @@
+//String
+ let firstName, companyName
+ firstName=`Bhagyalakshmi`
+ companyName=`Accenture`
+ console.log(firstName + ' is ' + typeof(firstName))
+ console.log(companyName + ' is ' + typeof(companyName))
+
+ //number
+ let mobileNumber= 9738297457
+ console.log(mobileNumber + ' is'+ typeof(mobileNumber))
+
+ //boolen
+ let isAutomation= true
+ console.log(isAutomation + ' is '+ typeof(isAutomation))
+
+ //Undefeined
+ let hasPlaywright
+ console.log(hasPlaywright + ' is '+ typeof(hasPlaywright))
---
+++ firstprogram.js
@@ -0,0 +1,3 @@
+console.log('Hello World')
+console.log('Bhagyalakshmi')
+console.log(3+3)
